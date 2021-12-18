8dc65b6ac374a4e6c175f52c80d0aabafda0d392
HarshBarash <barashEU@yandex.ru>
2021-12-18 11:01:24 +0500

    First custom GET Request

--- app/src/main/java/harshbarash/github/retrofit/api/SimpleApi.kt
+++ app/src/main/java/harshbarash/github/retrofit/api/SimpleApi.kt
@@ -3,8 +3,15 @@ package harshbarash.github.retrofit.api
 import harshbarash.github.retrofit.model.Post
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface SimpleApi {
     @GET("posts/1")
     suspend fun getPost(): Response<Post>
+
+    @GET("posts/{postNumber}")
+    suspend fun getPost2(
+        @Path("postNumber") number: Int
+    ): Response<Post>
+
 }
--- app/src/main/java/harshbarash/github/retrofit/repository/Repository.kt
+++ app/src/main/java/harshbarash/github/retrofit/repository/Repository.kt
@@ -7,6 +7,11 @@ import retrofit2.Retrofit
 
 class Repository {
     suspend fun getPost(): Response<Post> {
-       return RetrofitInstance.api.getPost()
+        return RetrofitInstance.api.getPost()
     }
+
+    suspend fun getPost2(number: Int): Response<Post> {
+        return RetrofitInstance.api.getPost2(number)
+    }
+
 }
