a3b202e50853179504659297de8e2b56b35bfb0a
HarshBarash <barashEU@yandex.ru>
2021-12-23 23:16:52 +0500

    create model Job for Rails

--- app/src/main/java/harshbarash/github/retrofit/utils/Constants.kt
+++ app/src/main/java/harshbarash/github/retrofit/utils/Constants.kt
@@ -3,6 +3,7 @@ package harshbarash.github.retrofit.utils
 class Constants {
 
     companion object {
-        const val  BASE_URL = "https://jsonplaceholder.typicode.com"
+        // const val  BASE_URL = "https://jsonplaceholder.typicode.com" //откладка placeholder
+        const val  BASE_URL = "http://localhost:3000"
     }
 }
