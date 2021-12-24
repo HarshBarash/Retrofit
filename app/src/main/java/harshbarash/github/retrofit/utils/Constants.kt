d12ae7c6a620ee5e4a6b8a39db5fd7fc4597c1b3
HarshBarash <barashEU@yandex.ru>
2021-12-24 23:43:20 +0500

    bugs fixed and updated theme

--- app/src/main/java/harshbarash/github/retrofit/utils/Constants.kt
+++ app/src/main/java/harshbarash/github/retrofit/utils/Constants.kt
@@ -4,6 +4,6 @@ class Constants {
 
     companion object {
         // const val  BASE_URL = "https://jsonplaceholder.typicode.com" //откладка placeholder
-        const val  BASE_URL = "http://localhost:3000"
+        const val  BASE_URL = "http://localhost:3001"
     }
 }
